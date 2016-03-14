09ff2d945d51fa65ed077b3968b47c7dd4f07a78
Mike Mueller <mikemueller04@hotmail.com>
2016-03-13 23:04:11 -0400

    passed test_end_time_before_6pm

--- babysitter.rb
+++ babysitter.rb
@@ -1,9 +1,12 @@
-def calculatePay(start_time)
-  validateTime(start_time)
+def calculatePay(start_time, end_time)
+  validateTime(start_time, end_time)
 end
 
-def validateTime(start_time)
-  if start_time < 17 || start_time > 3
+def validateTime(start_time, end_time)
+  if start_time < 17 && start_time > 3
     return "start time must not be before 5pm or after 3am"
+  end
+  if end_time < 18
+    return "end time must not be before 6pm"
   end
 end
--- babysittertest.rb
+++ babysittertest.rb
@@ -5,13 +5,23 @@ require 'test/unit'
 class BabySitterTest < Test::Unit::TestCase
 
   def test_start_time_before_5pm
-    s = calculatePay(16)
-    assert_equal("start time must not be before 5pm or after 3am", s)
+    result = calculatePay(16, 10)
+    assert_equal("start time must not be before 5pm or after 3am", result)
   end
 
   def test_start_time_after_3am
-    s = calculatePay(4)
-    assert_equal("start time must not be before 5pm or after 3am", s)
+    result = calculatePay(4, 10)
+    assert_equal("start time must not be before 5pm or after 3am", result)
+  end
+
+  def test_start_is_correct
+    result = calculatePay(18, 18)
+    assert_equal(nil, result)
+  end
+
+  def test_end_time_before_6pm
+    result = calculatePay(17, 5)
+    assert_equal("end time must not be before 6pm", result)
   end
 
 end
