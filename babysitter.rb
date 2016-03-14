548f3fe076f23937efc36bad4b46cec6c1d5fe1a
Mike Mueller <mikemueller04@hotmail.com>
2016-03-14 10:11:06 -0400

    passed test_pay_12hr_start_to_bedtime

--- babysitter.rb
+++ babysitter.rb
@@ -1,12 +1,25 @@
-def calculatePay(start_time, end_time)
-  validateTime(start_time, end_time)
-end
 
-def validateTime(start_time, end_time)
-  if start_time < 17 && start_time > 3
-    return "start time must not be before 5pm or after 3am"
+class Babysitter
+
+@@start_time_pay=12
+@@bed_time=22
+
+  def calculatePay(start_time, end_time)
+    message = validateTime(start_time, end_time)
+    if message == nil
+      return @@start_time_pay * (@@bed_time - start_time)
+    else
+      return message
+    end
   end
-  if end_time < 18 && end_time > 4
-    return "end time must not be before 6pm or after 4am"
+
+  def validateTime(start_time, end_time)
+    if start_time < 17 && start_time > 3
+      return "start time must not be before 5pm or after 3am"
+    end
+    if end_time < 18 && end_time > 4
+      return "end time must not be before 6pm or after 4am"
+    end
   end
+
 end
--- babysittertest.rb
+++ babysittertest.rb
@@ -1,36 +1,43 @@
 require_relative 'babysitter'
 require 'test/unit'
 
-
 class BabySitterTest < Test::Unit::TestCase
 
+  #babysitter = ::Babysitter.new
+
   def test_start_time_before_5pm
-    result = calculatePay(16, 10)
+    babysitter = Babysitter.new
+    result = babysitter.calculatePay(16, 10)
     assert_equal("start time must not be before 5pm or after 3am", result)
   end
 
   def test_start_time_after_3am
-    result = calculatePay(4, 10)
+    babysitter = Babysitter.new
+    result = babysitter.calculatePay(4, 10)
     assert_equal("start time must not be before 5pm or after 3am", result)
   end
 
   def test_start_is_correct
-    result = calculatePay(18, 18)
-    assert_equal(nil, result)
+    babysitter = Babysitter.new
+    result = babysitter.calculatePay(18, 18)
+    assert_not_equal("start time must not be before 5pm or after 3am", result)
   end
 
   def test_end_time_before_6pm
-    result = calculatePay(17, 17)
+    babysitter = Babysitter.new
+    result = babysitter.calculatePay(17, 17)
     assert_equal("end time must not be before 6pm or after 4am", result)
   end
 
   def test_end_time_after_4am
-    result = calculatePay(17, 5)
+    babysitter = Babysitter.new
+    result = babysitter.calculatePay(17, 5)
     assert_equal("end time must not be before 6pm or after 4am", result)
   end
 
   def test_pay_12hr_start_to_bedtime
-    result = calculatePay(17, 22)
+    babysitter = Babysitter.new
+    result = babysitter.calculatePay(17, 22)
     assert_equal(60, result)
   end
 
